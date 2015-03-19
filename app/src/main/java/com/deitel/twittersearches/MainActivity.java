e07f62275fb6cadb439ecb14b9986c41fdf752de
12301107-wangjunmeng <12301107@bjtu.edu.cn>
2015-03-19 20:02:54 +0800

    this is the Assignment two from 12301107 - Wang Junmeng

--- app/src/main/java/com/deitel/twittersearches/MainActivity.java
+++ app/src/main/java/com/deitel/twittersearches/MainActivity.java
@@ -135,7 +135,9 @@ public class MainActivity extends ListActivity
          // get query string and create a URL representing the search
          String tag = ((TextView) view).getText().toString();
          String urlString = getString(R.string.searchURL) +
-            Uri.encode(savedSearches.getString(tag, ""), "UTF-8");
+             Uri.encode(savedSearches.getString(tag, ""), "UTF-8");
+//          String urlString = Uri.encode(savedSearches.getString(tag, ""), "UTF-8");
+
          
          // create an Intent to launch a web browser    
          Intent webIntent = new Intent(Intent.ACTION_VIEW, 
